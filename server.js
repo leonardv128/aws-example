690d416e4c97dfd9943ebeec5293fcadbc345c01
leonardv128 <leonardv128@gmail.com>
2021-05-25 00:54:56 -0500

    mi quinto commit

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-app.get('/', (req, res) => res.send('<h1>TE AMO MUCHISISISISISISMOOOOOO!!</h1>'))
+app.get('/', (req, res) => res.send('<h1>PRUEBA NODE.JS SOBRE AWS</h1>'))
 
 app.listen(8080);
 console.log('Server on port 8080')
